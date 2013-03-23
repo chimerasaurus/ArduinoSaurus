308ba714c8a502b362b9507e2483ef60521e0015
James Malone <jamalone@gmail.com>
2013-03-23 10:24:06 -0700

    Fixes to work with IRRemote

--- GlowShow/Glowshow.h
+++ GlowShow/Glowshow.h
@@ -1,30 +1,30 @@
 /*
-  GlowShow.h - Library for static const unsigned interacting with fashionable headwear.
+  GlowShow.h - Library for static unsigned interacting with fashionable headwear.
   Created by James Malone, March 23, 2013.
-  Released static const unsigned into the public domain.
+  Released static unsigned into the public domain.
 */
 #ifndef Glowshow_h
 #define Glowshow_h
 	  
 	  /* CONSTANTS */
 	  // Values for changing both nodes
-	  static const unsigned int BOTH_BLUE[12] = {2085,417,834,834,417,417,1668,834,417,417,1668,2502}
-	  static const unsigned int BOTH_INACTIVE[14] = {2085,417,834,834,2502,834,417,417,834,834,834,417,417,834}
-	  static const unsigned int BOTH_GREEN[16] = {2085,417,834,834,834,417,1251,834,417,417,834,417,417,834,1251,417}
-	  static const unsigned int BOTH_YELLOW[16] = {2085,417,834,834,834,834,834,834,417,417,417,834,417,1668,417,417}
-	  static const unsigned int BOTH_CYAN[16] = {2085,417,834,834,417,834,1251,834,417,417,1251,417,1251,417,417,417}
-	  static const unsigned int BOTH_MAGENTA[18] = {2085,417,834,834,417,417,417,417,834,834,417,417,417,417,834,834,834,834}
-	  static const unsigned int BOTH_WHITE[18] = {2085,417,834,834,417,1251,834,834,417,417,417,417,417,417,834,417,834,417}
-	  static const unsigned int BOTH_RED[14] = {2085,417,834,834,1251,417,834,834,417,417,417,1251,1251,1251}
+	  static unsigned int BOTH_BLUE[12] = {2085,417,834,834,417,417,1668,834,417,417,1668,2502};
+	  static unsigned int BOTH_INACTIVE[14] = {2085,417,834,834,2502,834,417,417,834,834,834,417,417,834};
+	  static unsigned int BOTH_GREEN[16] = {2085,417,834,834,834,417,1251,834,417,417,834,417,417,834,1251,417};
+	  static unsigned int BOTH_YELLOW[16] = {2085,417,834,834,834,834,834,834,417,417,417,834,417,1668,417,417};
+	  static unsigned int BOTH_CYAN[16] = {2085,417,834,834,417,834,1251,834,417,417,1251,417,1251,417,417,417};
+	  static unsigned int BOTH_MAGENTA[18] = {2085,417,834,834,417,417,417,417,834,834,417,417,417,417,834,834,834,834};
+	  static unsigned int BOTH_WHITE[18] = {2085,417,834,834,417,1251,834,834,417,417,417,417,417,417,834,417,834,417};
+	  static unsigned int BOTH_RED[14] = {2085,417,834,834,1251,417,834,834,417,417,417,1251,1251,1251};
 	  
 	  // Values for changing one node (the right)
-	  static const unsigned int RIGHT_BLUE[12] = {2085,417,834,834,417,417,1668,834,417,417,1668,2502}
-	  static const unsigned int RIGHT_INACTIVE[14] = {2085,417,834,834,2502,834,417,417,834,834,834,417,417,834}
-	  static const unsigned int RIGHT_GREEN[16] = {2085,417,834,834,834,417,1251,834,417,417,834,417,417,834,1251,417}
-	  static const unsigned int RIGHT_YELLOW[16] = {2085,417,834,834,834,834,834,834,417,417,417,834,417,1668,417,417}
-	  static const unsigned int RIGHT_CYAN[16] = {2085,417,834,834,417,834,1251,834,417,417,1251,417,1251,417,417,417}
-	  static const unsigned int RIGHT_MAGENTA[18] = {2085,417,834,834,417,417,417,417,834,834,417,417,417,417,834,834,834,834}
-	  static const unsigned int RIGHT_WHITE[18] = {2085,417,834,834,417,1251,834,834,417,417,417,417,417,417,834,417,834,417}
-	  static const unsigned int RIGHT_RED[14] = {2085,417,834,834,1251,417,834,834,417,417,417,1251,1251,1251}
+	  static unsigned int RIGHT_BLUE[12] = {2085,417,834,834,417,417,1668,834,417,417,1668,2502};
+	  static unsigned int RIGHT_INACTIVE[14] = {2085,417,834,834,2502,834,417,417,834,834,834,417,417,834};
+	  static unsigned int RIGHT_GREEN[16] = {2085,417,834,834,834,417,1251,834,417,417,834,417,417,834,1251,417};
+	  static unsigned int RIGHT_YELLOW[16] = {2085,417,834,834,834,834,834,834,417,417,417,834,417,1668,417,417};
+	  static unsigned int RIGHT_CYAN[16] = {2085,417,834,834,417,834,1251,834,417,417,1251,417,1251,417,417,417};
+	  static unsigned int RIGHT_MAGENTA[18] = {2085,417,834,834,417,417,417,417,834,834,417,417,417,417,834,834,834,834};
+	  static unsigned int RIGHT_WHITE[18] = {2085,417,834,834,417,1251,834,834,417,417,417,417,417,417,834,417,834,417};
+	  static unsigned int RIGHT_RED[14] = {2085,417,834,834,1251,417,834,834,417,417,417,1251,1251,1251};
 
 #endif
